4d713a5799f071887ebb422acb5b7f16e47f2dd4
Lucianne Walkowicz <l.m.walkowicz@gmail.com>
2016-07-28 16:23:33 -0500

    modified web page display format to use html table

--- student_group_creator.py
+++ student_group_creator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import random
 from IPython.display import Image, HTML, display
+import webbrowser
 
 
 def showStudents(groups):
-    # would like to make student pictures display in their groups
+    # make student pictures display in their groups in an IPython notebook
     # NB: requires that image file names are the same as student names in the input list
 
     photoDir = './photodir/'
@@ -36,6 +37,49 @@ def showStudents(groups):
         
         imagesList=' '.join( fullImageDisplayTags )
         display(HTML(imagesList))
+
+def showStudentsInBrowser(groups):
+    # make student pictures display in their groups in a local browser window
+    # NB: requires that image file names are the same as student names in the input list
+    # default browser is chrome, preferred browser can be set by altering the below
+    browser_path = 'open -a /Applications/Google\ Chrome.app %s'
+    photoDir = './photodir/'
+    outFile = open("groups.html", "w")
+    
+    #create html to go before and after code generated for student groups
+    htmlPreamble = "<!DOCTYPE html><html><head><style>table {    font-family: arial, sans-serif;    border-collapse: collapse; } td, th {    border: 1px solid #dddddd;    text-align: center;    padding: 0px;} tr:nth-child(even) {    background-color: #dddddd;}</style></head><body><table>"
+    htmlClosing = "</table></body></html>"
+
+    #take each group list and parse student names into image file names (strip space, add .jpg)
+    outFile.write(htmlPreamble)
+    for i in range(0,len(groups)):
+        studentGroup = groups[i]
+        captionNames = [ ]
+        listOfImageNames = [ ]
+        for j in range(0,len(studentGroup)):
+            individualName = studentGroup[j]
+            individualName = individualName.replace(" ", "")
+            individualName+='.jpg'
+            fileName = photoDir+individualName
+            listOfImageNames.append(fileName)
+            # we also want the student name captions to be generated automatically from file names
+            captionNames.append(studentGroup[j])
+
+        #construct html to display each group member's image with their name
+        preFormatHTMLTags = ["<tr>"]
+        postFormatHTMLTags = ["</tr>"]
+        lineBreak = ["</tr><tr>"]
+        imageSources = [ "<td><img style='width: 200px; border: 1px solid black;' src='%s' /><td>"  % str(s) for s in listOfImageNames ]
+        captionSources = [ "<td><h1> %s </h1><td>"  % str(s) for s in captionNames ]
+        fullImageDisplayTags = preFormatHTMLTags + imageSources + lineBreak + captionSources + postFormatHTMLTags
+        imagesList=' '.join( fullImageDisplayTags )
+        outFile.write(imagesList)
+
+    #replace the below with writing the html file and displaying it in a browser window
+    outFile.write(htmlClosing)
+    outFile.close()
+    brwsr = webbrowser.get(browser_path)
+    brwsr.open_new('groups.html')
 
 def nameCounter(pars):
     #Parameters consists of numbers to indicate how many groups of each size should be created.
